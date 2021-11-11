1c953022f7c3f8995a4d3288b97714684e54129c
ori458 <94099307+ori458@users.noreply.github.com>
2021-11-11 15:23:20 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.forever(function () {
+    THK.モーター番号_角度(0, 50)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 0)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 0)
+})
